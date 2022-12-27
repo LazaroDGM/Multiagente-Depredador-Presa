5fccb94b66392d20cb6ed71b17190bb2cd185711
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2022-12-27 18:12:10 -0500

    add agent & enviroment

---
+++ simulator/environment.py
@@ -0,0 +1,38 @@
+class Environment():
+    '''
+    Clase abstracta para modelar los Medios Ambientes.
+    Un medio ambiente necesita una funcion de transformacion (`transform`) del Medio, y 
+    las funciones perceptuales (`see`) segun los tipos de agentes que pueden convivir en el
+    '''
+
+    def __init__(self) -> None:
+        self._see_functions = {}
+
+    def see(self, agent):
+        '''
+        Evaluacion de la funcion perceptual para un agente especifico. Dado el agente (`agent`),
+        y el tipo de agente (`type(agent)`), genera el conjunto de percepciones que
+        ese agente capatar'a, segun la funcion perceptual de su tipo.
+
+        `agent`: Instancia del agente
+
+        `return`: Conjunto de percepciones captadas
+        '''
+        return self._see_functions[type(agent)](agent)
+
+    def get_see_function(self, type_agent):
+        '''
+        Funcion perceptual para un agente especifico dado el tipo del agente (`type_agent`)
+
+        `type_agent`: Tipo de agente
+
+        `return`: Funcion perceptual
+        '''
+        return self._see_functions[type_agent]
+
+    def transform(self, actions):
+        '''
+        Funcion de transformacion del Medio, que dado un conjunto de acciones, 
+        cambia el estado del sistema
+        '''
+        raise NotImplementedError()
