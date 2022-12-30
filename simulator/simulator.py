4297babc7642672f8f5d339ff74fb22bbd85df05
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2022-12-30 13:20:25 -0500

    add 2d

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,3 +1,5 @@
+import time
+import pygame
 class Simulator():
     '''
     Clase generica de un Simulador por pasos de tiempo
@@ -37,7 +39,42 @@ class Simulator():
             simulations.append(outputs)
         return simulations
 
-    
+class Simulator2D(Simulator):
+
+    def __init__(self, ClassEnvironment) -> None:
+        super().__init__(ClassEnvironment)
+
+    def StartSimulation(self, tick, stop_steps, *args, **kvargs):
+        '''
+        Funcion que comienza la simulacion y genera un conjunto de salidas
+        que son las variables observables.
+
+        `stop_steps`: Minima cantidad de unidades de tiempo que deben ocurrir en la simulacion
+        para que esta finalice.
+
+        `*args`: Parametros posicionales de entrada del Medio
+        `**kvargs`: Parametros llave-valor de entrada del Medio
+
+        `return`: Tupla de 2 elementos, donde:
+            `return[0]`: La instancia del medio ambiente actual
+            `return[1]`: El conjunto de variables observables
+        '''
+        environment = self.class_enviroment(*args, **kvargs)
+        outputs = []
+        step = 0
+        pygame.init()
+        h, w = environment.shape_map
+        screen = pygame.display.set_mode((w * 40, h * 40))
+        while step < stop_steps:
+            environment.next_step()
+            outputs.append(environment.outputs())
+            self.draw(environment, screen)
+            time.sleep(tick)
+            step += 1
+        return environment, outputs
+
+    def draw(self, environment, screen):
+        raise NotImplementedError()
 
 
 
