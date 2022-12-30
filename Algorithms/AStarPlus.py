6494f960cbc8c96a7d09ca1647bc2bc1bd7d1065
francisco <franciscoayracaceress@gmail.com>
2022-12-30 17:03:30 -0500

    fixed una linea ahi No influye en nada pero es como deberia ser ja

--- Algorithms/AStarPlus.py
+++ Algorithms/AStarPlus.py
@@ -46,7 +46,7 @@ def AStarPlus(numpy_array, x, y, vision, found, obstacle):
         
     # Adding obstacles
     for i, j in positions:
-        if x + i - 1 not in range(0, len(numpy_array)) or y + j - 1 not in range(0, len(numpy_array[i])): 
+        if x + i - 1 not in range(0, len(numpy_array)) or y + j - 1 not in range(0, len(numpy_array[x + i - 1])):
             matrix[i][j].append(-1)
             continue
         print(f'position analized: {(x + i - 1, y + j - 1)}')
