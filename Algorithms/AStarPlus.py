277c6f578668551fb12b43717fac58a2a9f4d91f
francisco <franciscoayracaceress@gmail.com>
2023-01-02 02:16:09 -0500

    add A* plus

--- Algorithms/AStarPlus.py
+++ Algorithms/AStarPlus.py
@@ -1,4 +1,5 @@
 from heapq import heapify, heappop, heappush
+import math
 
 
 positions = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)]
@@ -13,18 +14,22 @@ def AStarPlus(numpy_array, x, y, vision, found, obstacle):
     heapify(heap)
     visited_cells = set()
     
-    heappush(heap, (0, (0, (0, (x, y)))))                       # 1- costo, 2- direccion de partida del camino, 3- dist. manhathan, 4- posicion correspondiente
+    heappush(heap, (0, (0, (0, ([], (x, y))))))                       # 1- costo, 2- direccion de partida del camino, 3- dist. manhathan, 4- camino o trace hsat el objetivo, 5- posicion correspondiente
     visited_cells.add((x, y))
+
+    min_d_way = math.inf
+    the_way = []
     
     while len(heap) != 0:
         min_tuple = heappop(heap)
         current_cost = min_tuple[0] - min_tuple[1][1][0]
         current_direction = min_tuple[1][0]
+        current_way = min_tuple[1][1][1][0]
         for j in range(-1, 2):
-            _x_pos = min_tuple[1][1][1][0] + j
+            _x_pos = min_tuple[1][1][1][1][0] + j
             if _x_pos < 0 or _x_pos >= len_x or abs(_x_pos - x) > vision: continue                                                  # out of range test
             for k in range(-1, 2):
-                _y_pos = min_tuple[1][1][1][1] + k
+                _y_pos = min_tuple[1][1][1][1][1] + k
                 if _y_pos < 0 or _y_pos >= len_y  or  (j == 0 and k == 0)  or  abs(_x_pos - x) > vision: continue       # out of range test
                 cell = _x_pos, _y_pos
                 if cell in visited_cells: continue                                                                                                             # cell already visited
@@ -40,17 +45,20 @@ def AStarPlus(numpy_array, x, y, vision, found, obstacle):
                             matrix[j + 1][k + 1].append(1)
                         else: 
                             matrix[int((current_direction - 1) / 3)][(current_direction - 1) % 3].append(current_cost + 1)
+                            if min_d_way > current_cost + 1:
+                                min_d_way = current_cost + 1
+                                the_way = current_way + [cell]
                 if obstacle_in_cell: continue
                 next_direction = (j+1)*3 + k+2       if          current_direction == 0        else            current_direction
-                heappush(heap, (manhathan_distance + current_cost + 1, (next_direction, (manhathan_distance, (_x_pos, _y_pos)))))
+                heappush(heap, (manhathan_distance + current_cost + 1, (next_direction, (current_way + [cell], (manhathan_distance, cell)))))
         
     # Adding obstacles
     for i, j in positions:
-        if x + i - 1 not in range(0, len(numpy_array)) or y + j - 1 not in range(0, len(numpy_array[x + i - 1])):
-            matrix[i][j].append(-1)
+        if x + i - 1 not in range(0, len(numpy_array)) or y + j - 1 not in range(0, len(numpy_array[x + i - 1])): 
+            matrix[i][j] = [-1]
             continue
         #print(f'position analized: {(x + i - 1, y + j - 1)}')
         for ent in numpy_array[x + i - 1][y + j - 1]:
             if obstacle(ent):
-                matrix[i][j].append(-1)
-    return matrix
+                matrix[i][j] = [-1]
+    return matrix, the_way
