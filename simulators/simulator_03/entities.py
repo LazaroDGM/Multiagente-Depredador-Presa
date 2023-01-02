1d69a4392b8bdc1a626e1b3d6088e9e314d05c9a
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-02 00:42:00 -0500

    add Burrow & Floor

--- simulators/simulator_03/entities.py
+++ simulators/simulator_03/entities.py
@@ -31,3 +31,79 @@ class Plant(object):
     '''
     def __init__(self) -> None:
         pass
+
+class Burrow():
+
+    def __init__(self) -> None:
+        self.agent = None
+
+    def isEmpty(self):
+        return self.agent is None
+
+    def Add(self, agent):
+        if not self.isEmpty():
+            raise Exception('Annadiendo agente en una madriguera ocupada')
+        self.agent = agent
+
+    def Remove(self):
+        if self.isEmpty():
+            raise Exception('Eliminando agente de una madriguera vacia')
+        agent = self.agent
+        self.agent = None
+        return agent
+
+class Floor(object):
+
+    def __init__(self) -> None:
+        self.food = None
+        self.prey = None
+        self.predator = None
+
+    def isEmpty(self):
+        return self.food is None and self.prey is None and self.predator is None
+
+    def hasAgent(self):
+        return self.prey is not None or self.predator is not None
+
+    def hasPrey(self):
+        return self.prey is not None
+
+    def hasPredator(self):
+        return self.predator is not None
+    
+    def hasFood(self):
+        return self.food is None
+
+    def AddFood(self):
+        if self.hasFood():
+            raise Exception('Annadiendo comida en suelo con comida')
+        self.food = Food()
+
+    def AddPrey(self, prey):
+        if self.hasPrey():
+            raise Exception('Annadiendo una presa en suelo con presa')
+        self.prey = prey
+
+    def AddPredator(self, predator):
+        if self.hasPredator():
+            raise Exception('Anndiendo un depredador a una casilla con depredador')
+        self.predator = predator
+
+    def RemoveFood(self):
+        if not self.hasFood():
+            raise Exception('Eliminando comida de un suelo sin comida')
+        self.food = None
+
+    def RemovePrey(self):
+        if not self.hasPrey():
+            raise Exception('Eliminando presa de un suelo sin presa')
+        agent = self.prey
+        self.prey = None
+        return agent
+
+    def RemovePredator(self):
+        if not self.hasPredator():
+            raise Exception('Eliminando depredador de un suelo sin depredador')
+        agent = self.predator
+        self.predator = None
+        return agent
